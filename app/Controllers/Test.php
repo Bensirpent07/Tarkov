c0b5df0ca200d6c78dbaa7487b1427e90401f978
Ben <benkuhman@gmail.com>
2020-08-27 17:29:47 -0500

    Updated to 0.0.5 updateTarkovDatabase.php has been rewritten to be more dynamic. Has been rewritten to utilize MediaWiki API.

--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -2,11 +2,13 @@
 
 
 namespace App\Controllers;
+use PDO;
 
 
 class Test extends BaseController
 {
-    public function index(){
-        echo "You're on the test page.";
+    public function index()
+    {
+        echo "You're on the test page. Beware below! <br><br>";
     }
 }
