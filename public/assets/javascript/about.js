f10ef607eee9d380afc01b081a8c4c7f65aaa157
Ben <benkuhman@gmail.com>
2020-08-30 05:12:14 -0500

    Optimized javascript.

--- public/assets/javascript/about.js
+++ public/assets/javascript/about.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-	$('div#imgContainer').css('transform', 'translateX(0)');
-	$('div#imgContainer').css('opacity', '100');
+	$('div#imgContainer').css({'transform': 'translateX(0)', 'opacity': '100'});
 
 	$('.slideLeft').each(function(index){
-		var self = this;
+		let self = this;
 		setTimeout(function(){
-			$(self).css('transform', 'translateX(0)');
-			$(self).css('opacity', '100');
+			$(self).css({'transform': 'translateX(0)', 'opacity': '100'});
 		}, index*1000);
 	});
 });
